277578e77fc150d7639b0ed348cbad7dd80e2ebd
yulong56 <956154853@qq.com>
2018-10-10 21:13:32 +0800

    12

--- app/Http/Controllers/API/WeChatController.php
+++ app/Http/Controllers/API/WeChatController.php
@@ -37,7 +37,7 @@ class WeChatController extends Controller
     {
         Log::info(__METHOD__ . " " . 'request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
         $app = app('wechat.official_account');
-        $response = $app->server->push(function ($message) {
+        $app->server->push(function ($message) {
             $app = app('wechat.official_account');
 
             //根据消息类型分别进行处理
@@ -86,6 +86,7 @@ class WeChatController extends Controller
                     break;
             }
         });
+        $response = $app->server->serve();
         return $response;
     }
 
