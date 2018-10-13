ed3749a78ef9f9e8449b51f331dd35afae5dc0cb
yulong56 <956154853@qq.com>
2018-10-13 11:39:36 +0800

    123

--- app/Http/Controllers/API/WeChatController.php
+++ app/Http/Controllers/API/WeChatController.php
@@ -18,8 +18,8 @@ use EasyWeChat\Kernel\Messages\Text;
 use EasyWeChat\Kernel\Messages\Video;
 use EasyWeChat\Kernel\Messages\Voice;*/
 use Illuminate\Support\Facades\Log;
-/*use Yansongda\Pay\Pay;*/
-use Illuminate\Http\Request;
+/*use Yansongda\Pay\Pay;
+use Illuminate\Http\Request;*/
 
 
 class WeChatController extends Controller
@@ -91,58 +91,8 @@ class WeChatController extends Controller
     }
     public function template(Request $request)
     {
-        Log::info(__METHOD__ . " " . 'request arrived.');
         $app = app('wechat.official_account');
-        $app->server->push(function ($message) {
-            $app = app('wechat.official_account');
-
-            //根据消息类型分别进行处理
-            switch ($message['MsgType']) {
-                case 'event':
-                    //点击事件
-                    if ($message['Event'] == 'CLICK') {
-                        switch ($message['EventKey']) {
-
-                        }
-                    }
-                    //关注事件
-                    if ($message['Event'] == 'subscribe') {
-
-                    }
-                    //取消关注事件
-                    if ($message['Event'] == 'unsubscribe') {
-
-                    }
-                    //扫描进入事件
-                    if ($message['Event'] == 'SCAN') {
-
-                    }
-                    break;
-                case 'text':        //文本消息
-
-                    break;
-                case 'image':
-
-                    break;
-                case 'voice':
-
-                    break;
-                case 'video':
-
-                    break;
-                case 'location':
-
-                    break;
-                case 'link':
-
-                    break;
-                // ... 其它消息
-                default:
-//                    return '';
-                    break;
-            }
-        });
-     $res = $app->template_message->sendSubscription([
+        $res = $app->template_message->sendSubscription([
             'touser' => 'o6DOM1Uunt_-vtxsbNtEby933COY',
             'template_id' => 'C_USwh9Af6oPS5db1g-M4Bsozk7BBpIGKjLc5RPUAWE',
             'url' => 'http://foryulong.isart.me',
