fe5560600cbbf8e0127ffdc93c0cb434ae80126b
yulong56 <956154853@qq.com>
2018-10-13 12:32:41 +0800

    1

--- app/Http/Controllers/API/WeChatController.php
+++ app/Http/Controllers/API/WeChatController.php
@@ -35,11 +35,17 @@ class WeChatController extends Controller
      */
     public function serve()
     {
+
         Log::info(__METHOD__ . " " . 'request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
         $app = app('wechat.official_account');
         $app->server->push(function ($message) {
-            $app = app('wechat.official_account');
-
+            $app = app(self::ACCOUNT_CONFIG);
+            Log::info(__METHOD__ . " " . "server receive:" . json_encode($message));
+            $user_openid = $message['FromUserName'];  //用户公众号openid
+            Log::info(__METHOD__ . " " . 'user_openid:' . $user_openid);
+            $wechat_user = $app->user->get($user_openid);        //通过用户openid获取信息
+            Log::info(__METHOD__ . " " . 'wechat_user:' . json_encode($wechat_user));
             //根据消息类型分别进行处理
             switch ($message['MsgType']) {
                 case 'event':
@@ -63,6 +69,13 @@ class WeChatController extends Controller
                     }
                     break;
                 case 'text':        //文本消息
+                    Log::info(__METHOD__ . " " . "message:" . json_encode($message));
+                    $text = $message['Content'];
+                    Log::info(__METHOD__ . " " . "text:" . $text);
+                    $text_msg = new Text($text);
+                    $app->customer_service->message($text_msg)
+                        ->to($user_openid)
+                        ->send();
 
                     break;
                 case 'image':
@@ -80,14 +93,60 @@ class WeChatController extends Controller
                 case 'link':
 
                     break;
-                // ... 其它消息
-                default:
-//                    return '';
-                    break;
-            }
-        });
-        $response = $app->server->serve();
-        return $response;
+
+//        Log::info(__METHOD__ . " " . 'request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+//        $app = app('wechat.official_account');
+//        $app->server->push(function ($message) {
+//            $app = app('wechat.official_account');
+//
+//            //根据消息类型分别进行处理
+//            switch ($message['MsgType']) {
+//                case 'event':
+//                    //点击事件
+//                    if ($message['Event'] == 'CLICK') {
+//                        switch ($message['EventKey']) {
+//
+//                        }
+//                    }
+//                    //关注事件
+//                    if ($message['Event'] == 'subscribe') {
+//
+//                    }
+//                    //取消关注事件
+//                    if ($message['Event'] == 'unsubscribe') {
+//
+//                    }
+//                    //扫描进入事件
+//                    if ($message['Event'] == 'SCAN') {
+//
+//                    }
+//                    break;
+//                case 'text':        //文本消息
+//
+//                    break;
+//                case 'image':
+//
+//                    break;
+//                case 'voice':
+//
+//                    break;
+//                case 'video':
+//
+//                    break;
+//                case 'location':
+//
+//                    break;
+//                case 'link':
+//
+//                    break;
+//                // ... 其它消息
+//                default:
+////                    return '';
+//                    break;
+//            }
+//        });
+//        $response = $app->server->serve();
+//        return $response;
     }
     public function template(Request $request)
     {
