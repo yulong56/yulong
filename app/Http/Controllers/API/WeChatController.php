e24d18edb3efd4f15f19c8a32b37232b6081ebb7
yulong56 <956154853@qq.com>
2018-10-13 11:28:52 +0800

    测试

--- app/Http/Controllers/API/WeChatController.php
+++ app/Http/Controllers/API/WeChatController.php
@@ -37,55 +37,55 @@ class WeChatController extends Controller
     {
         Log::info(__METHOD__ . " " . 'request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
         $app = app('wechat.official_account');
-        $app->server->push(function ($message) {
-            $app = app('wechat.official_account');
-
-            //根据消息类型分别进行处理
-            switch ($message['MsgType']) {
-                case 'event':
-                    //点击事件
-                    if ($message['Event'] == 'CLICK') {
-                        switch ($message['EventKey']) {
-
-                        }
-                    }
-                    //关注事件
-                    if ($message['Event'] == 'subscribe') {
-
-                    }
-                    //取消关注事件
-                    if ($message['Event'] == 'unsubscribe') {
-
-                    }
-                    //扫描进入事件
-                    if ($message['Event'] == 'SCAN') {
-
-                    }
-                    break;
-                case 'text':        //文本消息
-
-                    break;
-                case 'image':
-
-                    break;
-                case 'voice':
-
-                    break;
-                case 'video':
-
-                    break;
-                case 'location':
-
-                    break;
-                case 'link':
-
-                    break;
-                // ... 其它消息
-                default:
-//                    return '';
-                    break;
-            }
-        });
+//        $app->server->push(function ($message) {
+//            $app = app('wechat.official_account');
+//
+//            //根据消息类型分别进行处理
+//            switch ($message['MsgType']) {
+//                case 'event':
+//                    //点击事件
+//                    if ($message['Event'] == 'CLICK') {
+//                        switch ($message['EventKey']) {
+//
+//                        }
+//                    }
+//                    //关注事件
+//                    if ($message['Event'] == 'subscribe') {
+//
+//                    }
+//                    //取消关注事件
+//                    if ($message['Event'] == 'unsubscribe') {
+//
+//                    }
+//                    //扫描进入事件
+//                    if ($message['Event'] == 'SCAN') {
+//
+//                    }
+//                    break;
+//                case 'text':        //文本消息
+//
+//                    break;
+//                case 'image':
+//
+//                    break;
+//                case 'voice':
+//
+//                    break;
+//                case 'video':
+//
+//                    break;
+//                case 'location':
+//
+//                    break;
+//                case 'link':
+//
+//                    break;
+//                // ... 其它消息
+//                default:
+////                    return '';
+//                    break;
+//            }
+//        });
         $response = $app->server->serve();
         return $response;
     }
